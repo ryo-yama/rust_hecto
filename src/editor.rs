3daf8b8e3869c1d5ee6f418907730831b77e0754
ryo-yama <johnfun.4649.aa@gmail.com>
2024-11-12 23:38:54 +0900

    "Ctrl+Q" でやめれるようにした

--- src/editor.rs
+++ src/editor.rs
@@ -1,39 +1,47 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyEventKind, KeyModifiers};
 use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 
 
 pub struct Editor {
-
+	exit_editor: bool,
 }
 
 impl Editor {
 	pub fn default() -> Self {
-		Editor{}
+		Editor{exit_editor: false}
 	}
 
-	pub fn run(&self) {
+	pub fn run(&mut self) {
+		if let Err(err) = self.repl() {
+			panic!("{err:?}");
+		}
+		println!("GoodBye!\r\n");
+	}
+
+	fn repl(&mut self) -> Result<(), std::io::Error> {
 		// raw mode にする．
-		enable_raw_mode().unwrap();
+		enable_raw_mode()?;
 
 		loop {
-			match read () {
-				Ok(Key(event)) => {
-					println!("{:?} \r", event);
-					match event.code {
-						Char(input_char) => {
-							if  'q' == input_char {
-								break;
-							}
-						},
-						_ => (),
+			if let Key(KeyEvent {code, modifiers, kind, state} ) =  read()? {
+				if kind == KeyEventKind::Press {
+					println!("Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r");
+				}
+				match code {
+					// Ctrl + "Q" で終了する．
+					Char('q') if modifiers == KeyModifiers::CONTROL => {
+						self.exit_editor = true;
 					}
-				},
-				Err(err) => println!("Error: {}", err),
-				_ => ()
+					_ => (),
+				}
+			}
+			if self.exit_editor {
+				break;
 			}
 		}
 
 		// raw mode を解除する．
-		disable_raw_mode().unwrap();
+		disable_raw_mode()?;
+		Ok(())
 	}
 }
